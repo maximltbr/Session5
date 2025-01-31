366ebc883993506663819ec6a54436c2bcb9e251
Maxim-Lichtenberger <mlichtenberg.ieu2023@student.ie.edu>
2025-01-31 11:10:42 +0100

    for examples

--- for_examples.py
+++ for_examples.py
@@ -5,12 +5,11 @@ s = 0
 for i in range(1,11):
     s += i
 
-print(s
+print(s)
 
-i = 0
+i = 1
 s = 0
 while i <= 10:
     s += i
     i += 1
-    print(i)
-print(s)
+print("sum using while is also", s)
